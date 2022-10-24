10653add1a409327afdc18eff937250a44eb0e36
Hyungrae Woo <rea1109@squares.ai>
2022-10-24 15:20:43 +0900

    dragover event update throttling

--- main.js
+++ main.js
@@ -1,23 +1,60 @@
-// drage event
-$('#drop-area').on('dragenter dragover',function(e){
-    e.preventDefault();
-    $(this).addClass('active');
-});
+let timer;
 
-$('#drop-area').on({
+// drag event , container
+$('.container').on({
+    dragenter : function(e){
+        e.preventDefault();
+        $('#drop-area').addClass('active');
+        console.log('enter!');
+    },
+    dragover : function(e){
+        e.preventDefault();
+        if(!timer){
+            timer = setTimeout(()=>{
+                $('img').css('opacity','0.5');
+                $('#drop-area').addClass('active');
+                console.log('over!');
+                clearTimeout(timer);
+            },2000)
+        }
+    },
     drop : function(e){
-        dropImageFile(e);
+        e.preventDefault();
+        $('#drop-area').removeClass('active');
+        $('img').css('opacity','1');
+        const filelist = e.originalEvent.dataTransfer.files;
+        console.log('drop!');
+        console.log(filelist);
     },
     dragleave : function(e){
         e.preventDefault();
+        console.log('dragleave!');
         $(this).removeClass('active');
     }
 });
 
-function dropImageFile(e){
-    e.preventDefault();
-    $('#drop-area').removeClass('active');
-
-    const filelist = e.originalEvent.dataTransfer.files;
-    console.log(filelist);
-}
+// drag event , target area
+// $('#drop-area').on({
+//     dragenter : function(e){
+//         e.preventDefault();
+//         $('#drop-area').addClass('active');
+//         console.log('enter!');
+//     },
+//     dragover : function(e){
+//         e.preventDefault();
+//         $('#drop-area').addClass('active');
+//         console.log('over!');
+//     },
+//     drop : function(e){
+//         e.preventDefault();
+//         $('#drop-area').removeClass('active');
+//         const filelist = e.originalEvent.dataTransfer.files;
+//         console.log('drop!');
+//         console.log(filelist);
+//     },
+//     dragleave : function(e){
+//         e.preventDefault();
+//         console.log('dragleave!');
+//         $(this).removeClass('active');
+//     }
+// });
